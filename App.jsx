e0b61c3bb207c27a496064d599a52d590a0ade78
Bensam02 <71168586+Bensam02@users.noreply.github.com>
2021-06-28 08:24:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Information";
+import Information from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Footer />
+    </div>
+  );
+}
+export default App;
